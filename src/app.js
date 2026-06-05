cbb4a538a247dcde01486435f781ca7f70a08097
Amr2231 <amr540290@gmail.com>
2026-06-06 02:43:16 +0300

    end

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(
       "http://127.0.0.1:5173",
       "http://localhost:3001",
       "http://localhost:3000",
-      "https://backend-final-production-7de8.up.railway.app",
+      "https://final-project-biqc.vercel.app"
     ],
 
     credentials: true,
