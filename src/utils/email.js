132b22ff88b7863e526d662270e4377800e7054b
Amr2231 <amr540290@gmail.com>
2026-06-06 06:18:18 +0300

    end

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,21 +1,39 @@
-const nodemailer = require("nodemailer");
-require("dotenv").config();
+// const nodemailer = require("nodemailer");
+// require("dotenv").config();
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  // port: 587,
-  port: 465,
-  secure: true,
-  family: 4, // force IPv4
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   // port: 587,
+//   port: 465,
+//   secure: true,
+//   family: 4, // force IPv4
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// exports.sendResetEmail = async (email, link) => {
+//   await transporter.sendMail({
+//     from: `"Radiology System" <${process.env.EMAIL_USER}>`,
+//     to: email,
+//     subject: "Password Reset",
+//     html: `
+//       <h3>Password Reset</h3>
+//       <p>Click the link below to reset your password:</p>
+//       <a href="${link}">${link}</a>
+//       <p>This link expires in 15 minutes.</p>
+//     `,
+//   });
+// };
+
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 exports.sendResetEmail = async (email, link) => {
-  await transporter.sendMail({
-    from: `"Radiology System" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: "Radiology System <onboarding@resend.dev>",
     to: email,
     subject: "Password Reset",
     html: `
